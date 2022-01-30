56bfbf13b9bdb940e94039e030ddcbc5cc1d3380
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 15:46:35 -0600

    feat: add `Nav` component to `pages` components

--- gatsby__client/src/pages/index.tsx
+++ gatsby__client/src/pages/index.tsx
@@ -1,6 +1,10 @@
+import Nav from '../components/Nav';
+
 export default function IndexPage(): JSX.Element {
   return (
     <main>
+      <Nav />
+
       <h1>Hey! I&apos;m the Index page</h1>
     </main>
   );
--- gatsby__client/src/pages/slicemasters.tsx
+++ gatsby__client/src/pages/slicemasters.tsx
@@ -1,6 +1,10 @@
+import Nav from '../components/Nav';
+
 export default function SliceMastersPage(): JSX.Element {
   return (
     <main>
+      <Nav />
+
       <h1>Hey! I&apos;m the SliceMasters page</h1>
     </main>
   );
