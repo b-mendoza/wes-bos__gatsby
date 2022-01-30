039db8991c8ffdb269fae4f67ac6a489280537ba
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 16:52:34 -0600

    feat: remove `Nav` and wrapper `main` tag from `pages` components
    Since the Wrapper component will be defined on the `browser` and `ssr` files from Gatsby, this will be automatically add the `MainLayout` component to every page

--- gatsby__client/src/pages/beers.tsx
+++ gatsby__client/src/pages/beers.tsx
@@ -1,11 +1,7 @@
-import Nav from '../components/Nav';
-
 export default function BeersPage(): JSX.Element {
   return (
-    <main>
-      <Nav />
-
+    <>
       <h1>Hey! I&apos;m the Beers page</h1>
-    </main>
+    </>
   );
 }
--- gatsby__client/src/pages/slicemasters.tsx
+++ gatsby__client/src/pages/slicemasters.tsx
@@ -1,11 +1,7 @@
-import Nav from '../components/Nav';
-
 export default function SliceMastersPage(): JSX.Element {
   return (
-    <main>
-      <Nav />
-
+    <>
       <h1>Hey! I&apos;m the SliceMasters page</h1>
-    </main>
+    </>
   );
 }
