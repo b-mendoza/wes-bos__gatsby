b4ed31c63b31b18b41e6a79bfae71074bc17cc55
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 19:41:13 -0600

    feat: improve readability of `Nav` component

--- gatsby__client/src/components/Nav/index.tsx
+++ gatsby__client/src/components/Nav/index.tsx
@@ -1,31 +1,33 @@
-import { Link } from 'gatsby';
-
 import { Routes } from '../../@types/shared';
+import Logo from '../Logo';
+
+import { StyledLink, StyledNav, StyledUl } from './styled';
+
+interface Route {
+  path: Routes;
+  text: string;
+}
+
+const routes: Route[] = [
+  { path: Routes.IndexPage, text: 'Hot Now' },
+  { path: Routes.Pizzas, text: 'Pizza Menu' },
+  { path: Routes.IndexPage, text: 'LOGO' },
+  { path: Routes.SliceMasters, text: 'SliceMasters' },
+  { path: Routes.Order, text: 'Order Ahead' },
+];
 
 export default function Nav(): JSX.Element {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to={Routes.IndexPage}>Hot Now</Link>
-        </li>
-
-        <li>
-          <Link to={Routes.Pizzas}>Pizza Menu</Link>
-        </li>
-
-        <li>
-          <Link to={Routes.IndexPage}>LOGO</Link>
-        </li>
-
-        <li>
-          <Link to={Routes.SliceMasters}>SliceMasters</Link>
-        </li>
-
-        <li>
-          <Link to={Routes.Order}>Order Ahead</Link>
-        </li>
-      </ul>
-    </nav>
+    <StyledNav>
+      <StyledUl>
+        {routes.map((route, index) => (
+          <li key={index}>
+            <StyledLink to={route.path}>
+              {route.text === 'LOGO' ? <Logo /> : route.text}
+            </StyledLink>
+          </li>
+        ))}
+      </StyledUl>
+    </StyledNav>
   );
 }
