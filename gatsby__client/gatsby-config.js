aa77af7ae025d8ea0c6332d5962efd7a054385ed
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 20:22:09 -0600

    feat: add `gatsby-plugin-styled-components` to `plugins` from Gatsby `config` file

--- gatsby__client/gatsby-config.js
+++ gatsby__client/gatsby-config.js
@@ -4,6 +4,12 @@
 
 const gatsbyConfig = {
   jsxRuntime: 'automatic',
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-styled-components',
+      options: { displayName: true },
+    },
+  ],
 };
 
 export default gatsbyConfig;
