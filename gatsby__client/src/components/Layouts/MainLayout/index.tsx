ea8bff027bdcfb6c771762a21d96a457892a098e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 19:42:06 -0600

    feat: add `styled` components to `MainLayout` component

--- gatsby__client/src/components/Layouts/MainLayout/index.tsx
+++ gatsby__client/src/components/Layouts/MainLayout/index.tsx
@@ -1,6 +1,8 @@
 import Footer from '../../Footer';
 import Nav from '../../Nav';
 
+import { StyledContent, StyledSiteBorder } from './styled';
+
 interface MainLayoutProps {
   children: React.ReactNode;
 }
@@ -9,12 +11,14 @@ export default function MainLayout(props: MainLayoutProps): JSX.Element {
   const { children } = props;
 
   return (
-    <>
-      <Nav />
+    <StyledSiteBorder>
+      <StyledContent>
+        <Nav />
 
-      <main>{children}</main>
+        <main>{children}</main>
 
-      <Footer />
-    </>
+        <Footer />
+      </StyledContent>
+    </StyledSiteBorder>
   );
 }
