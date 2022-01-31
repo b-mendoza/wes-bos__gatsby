d90f0059b4cd3500907a2ca5c65131e98291cd18
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 19:41:32 -0600

    feat: add `styled` components of `MainLayout` component

---
+++ gatsby__client/src/components/Layouts/MainLayout/styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledContent = styled.div`
+  background-color: white;
+
+  padding: 1em;
+`;
+
+export const StyledSiteBorder = styled.div`
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+
+  max-width: 100rem;
+`;
