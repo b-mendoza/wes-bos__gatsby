cde0ac40ac52bce0f1012025fe72b8f822d81be7
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-30 19:43:15 -0600

    feat: add `StrictMode` to Gatsby `ssr` and `browser` config files

--- gatsby__client/gatsby-browser.js
+++ gatsby__client/gatsby-browser.js
@@ -1,11 +1,33 @@
 // @ts-check
 
+import 'normalize.css';
+import { StrictMode } from 'react';
+
 import MainLayout from './src/components/Layouts/MainLayout';
+
+import GlobalStyles from './src/styles/globals';
+import StyledTypography from './src/styles/typography';
 
 /** @type {import('gatsby').GatsbyBrowser['wrapPageElement']} */
 
 export const wrapPageElement = (wrapPageElementProps) => {
   const { element, props } = wrapPageElementProps;
 
-  return <MainLayout {...props}>{element}</MainLayout>;
+  return (
+    <>
+      <GlobalStyles />
+
+      <StyledTypography />
+
+      <MainLayout {...props}>{element}</MainLayout>
+    </>
+  );
+};
+
+/** @type {import('gatsby').GatsbyBrowser['wrapRootElement']} */
+
+export const wrapRootElement = (wrapRootElementProps) => {
+  const { element } = wrapRootElementProps;
+
+  return <StrictMode>{element}</StrictMode>;
 };
--- gatsby__client/gatsby-ssr.js
+++ gatsby__client/gatsby-ssr.js
@@ -1,11 +1,33 @@
 // @ts-check
 
+import 'normalize.css';
+import { StrictMode } from 'react';
+
 import MainLayout from './src/components/Layouts/MainLayout';
+
+import GlobalStyles from './src/styles/globals';
+import StyledTypography from './src/styles/typography';
 
 /** @type {import('gatsby').GatsbySSR['wrapPageElement']} */
 
 export const wrapPageElement = (wrapPageElementProps) => {
   const { element, props } = wrapPageElementProps;
 
-  return <MainLayout {...props}>{element}</MainLayout>;
+  return (
+    <>
+      <GlobalStyles />
+
+      <StyledTypography />
+
+      <MainLayout {...props}>{element}</MainLayout>
+    </>
+  );
+};
+
+/** @type {import('gatsby').GatsbySSR['wrapRootElement']} */
+
+export const wrapRootElement = (wrapRootElementProps) => {
+  const { element } = wrapRootElementProps;
+
+  return <StrictMode>{element}</StrictMode>;
 };
