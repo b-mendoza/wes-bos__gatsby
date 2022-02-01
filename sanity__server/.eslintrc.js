5bf7c820104dcb67258f300c0f6149a91472a87e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-31 22:58:58 -0600

    chore: remove `typescript` support from ESLint

--- sanity__server/.eslintrc.js
+++ sanity__server/.eslintrc.js
@@ -1,17 +1,8 @@
 module.exports = {
   env: { browser: true, es2021: true, node: true },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'standard-with-typescript',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ['standard', '@sanity/eslint-config-studio', 'prettier'],
   parserOptions: {
-    project: ['./tsconfig.json', './tsconfig.linters.json'],
     sourceType: 'module',
-    tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint'],
   root: true,
 };
