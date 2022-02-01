15b3d699e4644885522e42a629ccae1aaba8902e
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-01-31 23:02:47 -0600

    feat: add `pizza` type

---
+++ sanity__server/schemas/pizza.js
@@ -0,0 +1,44 @@
+import { MdLocalPizza } from '@react-icons/all-files/md/MdLocalPizza';
+
+const NAME_FIELD = {
+  description: 'Name of the Pizza',
+  name: 'name',
+  title: 'Pizza Name',
+  type: 'string',
+};
+
+const SLUG_FIELD = {
+  name: 'slug',
+  title: 'Slug',
+  type: 'slug',
+  options: {
+    maxLength: 100,
+    source: 'name',
+  },
+};
+
+const IMAGE_FIELD = {
+  description: 'The Image of the Pizza',
+  name: 'image',
+  title: 'Image',
+  type: 'image',
+  options: {
+    hotspot: true,
+  },
+};
+
+const NUMBER_FIELD = {
+  description: 'The Price of the Pizza',
+  name: 'price',
+  title: 'Price',
+  type: 'number',
+  validation: (Rule) => Rule.min(1000),
+};
+
+export default {
+  icon: MdLocalPizza,
+  name: 'pizza',
+  title: 'Pizza',
+  type: 'document',
+  fields: [NAME_FIELD, SLUG_FIELD, IMAGE_FIELD, NUMBER_FIELD],
+};
