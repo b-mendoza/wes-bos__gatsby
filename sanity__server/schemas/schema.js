5b8850a4bc7f61f0c386ced5c2ea02184545085d
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-02 21:13:53 -0600

    feat: add new types to schema

--- sanity__server/schemas/schema.js
+++ sanity__server/schemas/schema.js
@@ -1,9 +1,11 @@
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 import createSchema from 'part:@sanity/base/schema-creator';
 
-import pizzaSchema from './pizza';
+import personType from './person';
+import pizzaType from './pizza';
+import toppingType from './topping';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([pizzaSchema]),
+  types: schemaTypes.concat([personType, pizzaType, toppingType]),
 });
