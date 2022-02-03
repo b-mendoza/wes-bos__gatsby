a031c658c1e3bc44df2032058f8a0f0e51660697
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-02 22:56:05 -0600

    feat: add TypeScript support to ESLint

--- sanity__server/.eslintrc.js
+++ sanity__server/.eslintrc.js
@@ -1,8 +1,27 @@
 module.exports = {
   env: { browser: true, es2021: true, node: true },
-  extends: ['standard', '@sanity/eslint-config-studio', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/strict',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+
+    project: ['./tsconfig.json', './tsconfig.linters.json'],
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
   },
   root: true,
+  plugins: ['@typescript-eslint', 'html'],
+  settings: {
+    react: { version: 'detect' },
+  },
 };
