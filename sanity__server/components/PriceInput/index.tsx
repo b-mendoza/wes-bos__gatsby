f6bfc96809afb04fb9c6a4ee2b0c98d7bbcedefb
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-03 22:51:21 -0600

    feat: update `PriceInput` component
    Change List
    
    - Add types
    - Add `createPatchEvent` function to submit the data back to Sanity
    - Add `ref` to control the `input` element

--- sanity__server/components/PriceInput/index.tsx
+++ sanity__server/components/PriceInput/index.tsx
@@ -1,7 +1,49 @@
+import PatchEvent, { set, unset } from '@sanity/form-builder/PatchEvent';
+import type { NumberSchemaType } from '@sanity/types';
 import React from 'react';
 
-const PriceInput = React.forwardRef(function PriceInput() {
-  return <h2>Pizza Price</h2>;
-});
+interface PriceInputProps {
+  type: Pick<NumberSchemaType, 'description' | 'name' | 'title'>;
+  value: string;
+  onChange: (event: PatchEvent) => void;
+}
 
-export default PriceInput;
+const createPatchEvent = (value: string) =>
+  PatchEvent.from(value.trim().length ? set(parseFloat(value)) : unset());
+
+function PriceInput(
+  props: PriceInputProps,
+  ref: React.ForwardedRef<HTMLInputElement>,
+) {
+  const { type, value, onChange } = props;
+
+  const inputChangeHandler = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const inputValue = event.target.value;
+
+      onChange(createPatchEvent(inputValue));
+    },
+    [onChange],
+  );
+
+  return (
+    <>
+      <h2>{type.title}</h2>
+
+      {type.description ? <p>{type.description}</p> : null}
+
+      <input
+        ref={ref}
+        type={type.name}
+        value={value}
+        onChange={inputChangeHandler}
+      />
+    </>
+  );
+}
+
+const PriceInputRef = React.forwardRef<HTMLInputElement, PriceInputProps>(
+  PriceInput,
+);
+
+export default PriceInputRef;
