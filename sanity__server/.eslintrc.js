2098ef910de1fa49b32a67b439fc0b027944ca31
Bryan Mendoza <bmendoza.dev@hotmail.com>
2022-02-03 22:45:54 -0600

    feat: update linter `extends` to the ones for `Airbnb`

--- sanity__server/.eslintrc.js
+++ sanity__server/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: { browser: true, es2021: true, node: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:jsx-a11y/strict',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/strict',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
